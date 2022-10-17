e5928b7519d692d8d249daaf00b449a076a34314
ErikBorgogno <erikborgo.20@gmail.com>
2022-10-17 16:19:45 +0200

    init

---
+++ resources/views/guests/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Boolpress</title>
+    <link rel="shortcut icon" href="/images/icons8-b-gradient-32.ico" type="image/x-icon">
+    <link rel="stylesheet" href="{{asset('css/app.css') }}">
+</head>
+<body>
+    <a href="{{route('admin.home')}}">Administration area</a>
+
+    <div id="root">
+    </div>
+
+    <script src="{{asset('js/front.js')}}"></script>
+    
+</body>
+</html>
